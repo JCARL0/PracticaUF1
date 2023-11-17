2cffb6cbdec82217e66132a5ab550616577856db
JCARL0 <john.magno@estudiant,fjaverianas.com>
2023-11-17 22:51:52 +0100

    Agregando un fichero ejecutable

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicitar al usuario que ingrese su nombre
+        Console.Write("Por favor, introduce tu nombre: ");
+        
+        // Leer el nombre del usuario desde la entrada estándar (teclado)
+        string nombre = Console.ReadLine();
+        
+        // Mostrar un mensaje de bienvenida en la pantalla
+        Console.WriteLine($"¡Bienvenido, {nombre}!");
+
+        // Esperar a que el usuario presione una tecla antes de cerrar la aplicación (opcional)
+        Console.ReadKey();
+    }
+}
